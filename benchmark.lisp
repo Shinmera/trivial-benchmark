23ec9481cb63c850aeea0bd3d382b2bafcafbd41
Shinmera <shinmera@tymoon.eu>
2015-01-15 21:41:09 +0100

    Cleanup and avoiding using APPLY where possible to make things not crash if you use high iteration counts.

--- benchmark.lisp
+++ benchmark.lisp
@@ -30,13 +30,13 @@
   "Computes statistical values from the list of numbers:
 TOTAL MAX MIN MEDIAN AVERAGE STANDARD-DEVIATION"
   (let* ((count (length nums))
-         (total (apply #'+ nums))
+         (total (reduce #'+ nums))
          (average (/ total count))
-         (variance (/ (apply #'+ (mapcar #'(lambda (a) (expt (- a average) 2)) nums)) count))
+         (variance (/ (reduce #'+ (mapcar #'(lambda (a) (expt (- a average) 2)) nums)) count))
          (deviation (sqrt variance))
          (median (elt (sort (copy-seq nums) #'<) (1- (ceiling (/ count 2)))))
-         (max (apply #'max nums))
-         (min (apply #'min nums)))
+         (max (reduce #'max nums))
+         (min (reduce #'min nums)))
     (list total max min median average deviation)))
 
 (defun print-table (table &key (stream T) (padding 2))
@@ -54,35 +54,51 @@ TOTAL MAX MIN MEDIAN AVERAGE STANDARD-DEVIATION"
 (defun format-stats (stream &rest data)
   (let ((list (cons '("-" "TOTAL" "MAXIMUM" "MINIMUM" "MEDIAN" "AVERAGE" "DEVIATION")
                     (loop for (key val) on data by #'cddr
-                          collect (cons key (mapcar #'(lambda (a) (format NIL "~f" (round-to a 6)))
-                                                    (compute-stats val)))))))
+                          collect (list* key (mapcar #'(lambda (a) (format NIL "~f" (round-to a 6)))
+                                                     (compute-stats val)))))))
     (print-table list :stream stream)))
 
 #+sbcl
-(defmacro with-timing (iterations &body body)
-  `(loop for i from 1 upto ,iterations
-         for result = (let ((var))
-                        (flet ((tmp (&key real-time-ms user-run-time-us system-run-time-us gc-run-time-ms
-                                       processor-cycles bytes-consed &allow-other-keys)
-                                 (setf var (list :real-time (float (/ real-time-ms 1000))
-                                                 :user-run-time (float (/ user-run-time-us 1000000))
-                                                 :system-run-time (float (/ system-run-time-us 1000000))
-                                                 :gc-run-time (float (/ gc-run-time-ms 1000))
-                                                 :processor-cycles processor-cycles
-                                                 :bytes-consed bytes-consed))))
-                          (sb-impl::call-with-timing #'tmp #'(lambda () ,@body)))
-                        var)
-         collect result into results
-         finally (apply #'format-stats T (apply #'map 'list #'(lambda (&rest a) (if (keywordp (car a)) (car a) a)) results))))
+(defun %time-function (function)
+  (let ((var))
+    (flet ((tmp (&key real-time-ms user-run-time-us system-run-time-us gc-run-time-ms
+                      processor-cycles bytes-consed &allow-other-keys)
+             (setf var (list (float (/ real-time-ms 1000))
+                             (float (/ user-run-time-us 1000000))
+                             (float (/ system-run-time-us 1000000))
+                             (float (/ gc-run-time-ms 1000))
+                             processor-cycles
+                             bytes-consed))))
+      (sb-impl::call-with-timing #'tmp function))
+    var))
+
+#+sbcl
+(defun call-with-timing (iterations function)
+  (loop for i from 1 upto iterations
+        for (rt urt srt grt pc bc) = (%time-function function)
+        collect rt into real-time
+        collect urt into user-run-time
+        collect srt into system-run-time
+        collect grt into gc-run-time
+        collect pc into processor-cycles
+        collect bc into bytes-consed
+        finally (format-stats T
+                              :real-time real-time
+                              :user-run-time user-run-time
+                              :system-run-time system-run-time
+                              :gc-run-time gc-run-time
+                              :processor-cycles processor-cycles
+                              :bytes-consed bytes-consed)))
 
 #-sbcl
-(defmacro with-timing (iterations &body body)
-  `(loop for i from 1 upto ,iterations
-         for (result real run) = (multiple-value-list (time-spent ,@body))
+(defun call-with-timing (iterations function)
+  `(loop for i from 1 upto iterations
+         for (result real run) = (multiple-value-list (time-spent (funcall function)))
          collect real into reals
          collect run into runs
          finally (format-stats T :REAL-TIME reals :RUN-TIME runs)))
 
-(setf (documentation 'with-timing 'function)
-      "Executes the body ITERATIONS times, collecting statistical data in the process.
-After completion prints a table of the collection information and returns NIL.")
+(defmacro with-timing (iterations &body body)
+  "Executes the body ITERATIONS times, collecting statistical data in the process.
+After completion prints a table of the collection information and returns NIL."
+  `(call-with-timing ,iterations #'(lambda () ,@body)))
