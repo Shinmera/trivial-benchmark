Shinmera/trivial-benchmark

commit 8514a3c3b41872303af4899ef59ccfc2b62a2955

Add REPORT-TO method for the package benchmark hash table

suite.lisp
@@ -17,6 +17,16 @@
         nil
         package-object)))
 
+(defmethod report-to ((stream stream) (hash-table hash-table) &key (computations *default-computations))
+  (dolist (result (alexandria:hash-table-alist hash-table))
+    (destructuring-bind (name &rest metrics) result
+      (format stream "Results for benchmark ~a~%" (string name))
+      (print-table (cons (cons :- computations)
+                         (loop :for metric :in metrics
+                               :collect (remove-if (lambda (elt) (find elt computations))
+                                                   metric))))
+      (format stream "~%"))))
+
 (defmacro define-benchmark-package (name &body package-options)
   "Define a package as if by DEFPACKAGE, except that both the COMMON-LISP and TRIVIAL-BENCHMARK packages are used, and that this package is designated as one containing benchmarks."
   `(progn

commit 2a0e2c73218ff6b325d783264813a44a1681eb1c

Typo in arglist of report-to.
Fixes regression introduced by 8514a3c3b41872303af4899ef59ccfc2b62a2955
Fixes #7

suite.lisp
@@ -17,7 +17,7 @@
         nil
         package-object)))
 
-(defmethod report-to ((stream stream) (hash-table hash-table) &key (computations *default-computations))
+(defmethod report-to ((stream stream) (hash-table hash-table) &key (computations *default-computations*))
   (dolist (result (alexandria:hash-table-alist hash-table))
     (destructuring-bind (name &rest metrics) result
       (format stream "Results for benchmark ~a~%" (string name))